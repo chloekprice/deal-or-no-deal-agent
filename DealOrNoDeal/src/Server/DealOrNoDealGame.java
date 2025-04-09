fbb03219bad88184a9ae42201e4584f010cf7b97
Chloe Brown <144487076+chloekprice@users.noreply.github.com>
2025-04-09 00:26:17 -0600

    add function stubs for server game logic

---
+++ DealOrNoDeal/src/Server/DealOrNoDealGame.java
@@ -0,0 +1,80 @@
+package Server;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.lang.*;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import java.io.*;
+import java.util.Random;
+
+import java.net.*;
+
+
+
+class TheStage extends JComponent {
+
+
+    public TheStage() {
+
+    }
+
+    public void updatePosition() {
+
+    }
+
+    public void display(Graphics g) {
+
+    }
+}
+
+public class DealOrNoDealGame extends JFrame {
+
+
+    public DealOrNoDealGame() {
+
+    }
+
+    private void getConnection(int port, String fnombre, String _known) {
+
+    }
+
+    void initRobotPosition() {
+
+    }
+
+    void moveIt() {
+
+    }
+
+    void moveRobot() {
+
+    }
+
+    // returns a strong with a char specifying north south east west; 1 = wall; 0 = no wall
+    String getSonarReadings() {
+        return "";
+    }
+
+    void survive() {
+
+    }
+
+
+    public static void main(String[] args) {
+        TheStage stage = new TheStage();
+    }
+}
+
